45758d9bc8985a9239f7ee1b1f367b374b2a939e
ppmalinsankesantrian-cell <ppmalinsan.kesantrian@gmail.com>
2026-06-08 12:39:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 
 const BASE44_APP_ID = "69eabbe48b257f7737097f4e";
@@ -11,58 +12,31 @@ app.get("/api-santri", async (req, res) => {
 
 const uid = req.query.uid;
 
-if (!uid) {
-return res.json({
-status: false,
-message: "UID tidak dikirim"
-});
-}
-
 try {
 
-```
 const response = await fetch(
-  `https://app.base44.com/api/apps/${BASE44_APP_ID}/entities/Student`
-,{
-  headers: {
-    "api_key": BASE44_API_KEY
+  `https://app.base44.com/api/apps/${BASE44_APP_ID}/entities/Student`,
+  {
+    headers: {
+      "api_key": BASE44_API_KEY
+    }
   }
-});
-
-const students = await response.json();
-
-const santri = students.find(
-  s => s.rfid_uid &&
-       s.rfid_uid.toUpperCase() === uid.toUpperCase()
 );
 
-if (santri) {
+const data = await response.text();
 
-  res.json({
-    uid: santri.rfid_uid,
-    nama: santri.name,
-    kelas: santri.kelas,
-    kamar: santri.kamar
-  });
-
-} else {
-
-  res.json({
-    status: false,
-    message: "Santri tidak ditemukan"
-  });
-
-}
-```
-
-} catch (error) {
-
-```
-res.json({
-  status: false,
-  message: error.message
+res.send({
+  status: true,
+  uid: uid,
+  debug: data
 });
-```
+
+} catch (err) {
+
+res.status(500).send({
+  status: false,
+  error: err.message
+});
 
 }
 
