e732b9588de41afd72126849acc93da339c0179f
ppmalinsankesantrian-cell <ppmalinsan.kesantrian@gmail.com>
2026-06-08 13:02:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
-module.exports = async (req, res) => {
+const https = require("https");
 
-try {
+module.exports = (req, res) => {
 
-```
-const response = await fetch(
-  "https://app.base44.com/api/apps/69eabbe48b257f7737097f4e/entities/Student",
-  {
+  const options = {
+    hostname: "app.base44.com",
+    path: "/api/apps/69eabbe48b257f7737097f4e/entities/Student",
+    method: "GET",
     headers: {
       api_key: "48e999e43f7d4b11bc3896afa62cd578"
     }
-  }
-);
+  };
 
-const data = await response.text();
+  const request = https.request(options, response => {
 
-return res.status(200).send(data);
-```
+    let data = "";
 
-} catch (err) {
+    response.on("data", chunk => {
+      data += chunk;
+    });
 
-```
-return res.status(500).json({
-  error: err.message
-});
-```
+    response.on("end", () => {
+      res.status(200).send(data);
+    });
 
-}
+  });
 
+  request.on("error", error => {
+    res.status(500).json({
+      error: error.message
+    });
+  });
+
+  request.end();
 };
